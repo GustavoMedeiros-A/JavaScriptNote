e56cf4194191dc84be791a5ecda1aaaf8291b1d3
Gustavo <gustavomedeiros15@outlook.com.br>
2023-03-29 14:33:24 -0300

    Create Login methods in the model, the routerPost to make the login and sign the JWT token

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const User = require("../models/user")
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); //Make the variables available to us
+const secret = process.env.JWT_TOKEN;
 
 
 router.post("/register", async(req, res) => {
   const { name, email, password } = req.body;
   const user = new User({ name, email, password });
-  
   try {
     await user.save();
     res.status(200).json({user:user});
   } catch (error) {
     res.status(500).json(error)
   }
+})
+
+router.post("/login", async (req, res) => {
+  const { email, password} = req.body;
+  const maxAge = 3 *24 *60 *60;
+  try {
+    const user = await User.login(email, password);
+    const token = jwt.sign({email}, secret, {expiresIn: maxAge})
+    res.json({user: user, token:token})
+  } catch (error) {
+    res.status(500).json({error: "Incorrect email or password"})
+  }
+
 })
 
 
